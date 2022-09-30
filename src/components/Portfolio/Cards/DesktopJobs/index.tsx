7d9122c4917dbee07a7ef9a00a4e4828b02e1c0d
Gustavo Leandro Gonçalves <guto_leandro95@hotmail.com.br>
2022-09-29 22:14:57 -0300

    update portfolio

--- src/components/Portfolio/Cards/DesktopJobs/index.tsx
+++ src/components/Portfolio/Cards/DesktopJobs/index.tsx
@@ -2,8 +2,6 @@ import styles from '../Cards.module.scss'
 
 export default function DesktopJobs() {
     return <div className={styles.cards}>
-        <img className={styles.cards__card} src={require('../../../.././assets/portfolio/desktopJobs/disk.png')} alt='website' />
         <img className={styles.cards__card} src={require('../../../.././assets/portfolio/desktopJobs/agile.png')} alt='website' />
-        <img className={styles.cards__card} src={require('../../../.././assets/portfolio/desktopJobs/stratto.png')} alt='website' />
     </div>
 }
