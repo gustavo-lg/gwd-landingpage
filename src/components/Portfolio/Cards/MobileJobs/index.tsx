11875d006ff95d1d79c315d2f33240cc3ed5ca7b
Gustavo Leandro Gonçalves <guto_leandro95@hotmail.com.br>
2022-11-19 07:51:14 -0300

    update portfolio and fix footer

--- src/components/Portfolio/Cards/MobileJobs/index.tsx
+++ src/components/Portfolio/Cards/MobileJobs/index.tsx
@@ -4,7 +4,12 @@ import examples from './examples.json';
 export default function MobileJobs() {
     return <div className={styles.cards}>
         {examples.map((links, index) => (
+            <div>
+                <h3 key={index} className={styles.cards__title}>{links.title}</h3>
+                <a href={links.link} target='_blank' rel="noreferrer">
                     <img key={index} className={styles.cards__card} src={require(`../../../.././assets/portfolio/mobileJobs/${links.src}`)} alt='website' />
+                </a>
+            </div>
         ))}
     </div>
 }
